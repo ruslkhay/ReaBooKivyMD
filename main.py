45948dcb242b112840f337ae316178aa056dde23
Ruslan Khaydarpashich <h.ruslan2000@yandex.ru>
2024-04-05 00:16:51 +0300

    Main window without events

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
 from kivymd.app import MDApp
 # from kivy.lang import Builder
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.floatlayout import MDFloatLayout
+# from kivymd.uix.screen import MDScreen
+# from kivymd.uix.button import MDButton
+# from kivy.lang import Builder
 
 # Builder.load_file("tracker.kv")
+
+from kivy.core.window import Window
+Window.size = (1080, 1920)
+
+
+class LanguageBar(MDFloatLayout):
+    pass
+
+
+class MainWindow(MDBoxLayout):
+    pass
 
 
 # Main App class
 class MainApp(MDApp):
     task_list_dialog = None
 
+class ReabooApp(MDApp):
     def build(self):
-        # Setting theme to my favorite theme
-        # self.theme_cls.theme_style = "Dark"
-        self.theme_cls.primary_palette = "Amber"
+        return MainWindow()
+
 
 
 if __name__ == "__main__":
     MainApp().run()
+
+if __name__ == "__main__":
+    ReabooApp().run()

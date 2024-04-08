5592aa91226bceecaee5adc4fb3dc148019e076d
Ruslan Khaydarpashich <h.ruslan2000@yandex.ru>
2024-04-08 13:35:16 +0300

    Dictionary screen without events
    There is no opportunity to switch between Main and Dictonary windows.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from kivymd.app import MDApp
 # from kivy.lang import Builder
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.floatlayout import MDFloatLayout
+
+from kivymd.uix.list import TwoLineAvatarIconListItem, IconRightWidget
 # from kivymd.uix.screen import MDScreen
 # from kivymd.uix.button import MDButton
 # from kivy.lang import Builder
@@ -13,25 +15,39 @@ Window.size = (1080, 1920)
 
 
 class LanguageBar(MDFloatLayout):
+    """Top bar with current language"""
     pass
 
 
 class MainWindow(MDBoxLayout):
+    """Initial loading window"""
     pass
 
 
-# Main App class
-class MainApp(MDApp):
-    task_list_dialog = None
+class DictionaryWindow(MDBoxLayout):
+    """Window for handling and mending dictionary """
+    pass
+
 
 class ReabooApp(MDApp):
+    """Main application"""
+
     def build(self):
-        return MainWindow()
+        """Returns the root of your widget tree"""
+        return DictionaryWindow()
 
+    def on_start(self):
+        for i in range(50):
+            self.root.ids.container.add_widget(
+                TwoLineAvatarIconListItem(
+                    IconRightWidget(
+                        icon="dots-vertical"
+                    ),
+                    text=f"item{i}",
+                    secondary_text=f"Square = {i**2}"
+                )
+            )
 
-
-if __name__ == "__main__":
-    MainApp().run()
 
 if __name__ == "__main__":
     ReabooApp().run()

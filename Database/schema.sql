c183dbf9ee801ecc3e84b731c4372c4ca3e5b413
Ruslan Khaydarpashich <h.ruslan2000@yandex.ru>
2024-04-29 13:20:02 +0300

    Delete content from table
    Now delete action is enabled in database. You can write specific
    "foreign" word and it's translation (row of "dictionary" table)
    into "DELETE FROM ..." query and "delete" column of "translate" table
    will be updated from '0' to '1'.

--- Database/schema.sql
+++ Database/schema.sql
@@ -5,6 +5,7 @@ CREATE TABLE IF NOT EXISTS "words"(
     PRIMARY KEY("id")
 );
 
+
 -- creates table for word's translations 
 CREATE TABLE IF NOT EXISTS "translations"(
     "id" INTEGER,
@@ -12,10 +13,12 @@ CREATE TABLE IF NOT EXISTS "translations"(
     PRIMARY KEY("id")
 );
 
--- binding between previous tables
+
+-- binding between previous tables.
 CREATE TABLE IF NOT EXISTS "translate"(
     "word_id" INTEGER,
     "translation_id" INTEGER,
+    "deleted" INTEGER DEFAULT 0,
     PRIMARY KEY ("word_id", "translation_id"),
     FOREIGN KEY("word_id") REFERENCES "words"("id")
     ON DELETE CASCADE,
@@ -23,11 +26,14 @@ CREATE TABLE IF NOT EXISTS "translate"(
     ON DELETE CASCADE
 );
 
+
 -- 'foreign' words next to their translation, i.e. dictionary itself
 CREATE VIEW IF NOT EXISTS "dictionary" AS 
 SELECT "name" AS 'word', "meaning" FROM "words"
 JOIN "translate" ON "words"."id" = "translate"."word_id"
-JOIN "translations" ON "translate"."translation_id" = "translations"."id";
+JOIN "translations" ON "translate"."translation_id" = "translations"."id"
+WHERE "translate"."deleted" != 1;
+
 
 -- auto-insert values into all tables only by given word and it's translation 
 CREATE TRIGGER IF NOT EXISTS "insert"
@@ -53,8 +59,42 @@ BEGIN
         )
     )
     ON CONFLICT ("word_id", "translation_id") DO NOTHING;
-
 END;
 
 
+-- auto-delete values from all tables by given word and it's translation.
+-- This trigger just marks row in "translate" table as deleted.
+CREATE TRIGGER IF NOT EXISTS "delete"
+INSTEAD OF DELETE ON "dictionary"
+FOR EACH ROW 
+BEGIN
+    UPDATE "translate" SET "deleted" = 1
+    WHERE "word_id" = (
+        SELECT "id" FROM "words"
+        WHERE "name" = (OLD."word")
+    ) AND "translation_id" = (
+        SELECT "id" FROM "translations"
+        WHERE "meaning" = (OLD."meaning")
+    );
+END;
+
+
+-- restore values if they were deleted
+CREATE TRIGGER IF NOT EXISTS "insert_when_exist"
+INSTEAD OF INSERT ON "dictionary"
+WHEN NEW."word" IN (
+    SELECT "name" FROM "words"
+) AND NEW."meaning" IN (
+    SELECT "meaning" FROM "translations"
+)
+BEGIN
+    UPDATE "translate" SET "deleted" = 0
+    WHERE "word_id" = (
+        SELECT "id" FROM "words"
+        WHERE "name" = (NEW."word")
+    ) AND "translation_id" = (
+        SELECT "id" FROM "translations"
+        WHERE "meaning" = (NEW."meaning")
+    );
+END;
 
